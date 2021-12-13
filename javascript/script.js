a39353acdc0c4a4fa66b15f857bd16602e8b8944
ROBY MARTINEZ <=MARTINEZ.ROBYP@gmail.com>
2021-12-13 11:35:03 -0700

    adds button functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,40 @@
-console.log("Roger Roger");
+$(function(){
+
+const URL = "https://www.googleapis.com/books/v1/volumes?q="
+
+
+const $input = $('input[type="text"]');
+const $button = $('form');
+
+$button.on('submit', bookSearch)
+
+// console.log($input)
+function bookSearch(evt) {
+    evt.preventDefault();
+    const userInput = $input.val();
+    $.ajax(URL + userInput).then(function (data){
+        render(data);
+    }, function (error) {
+        console.log(error);
+    }
+    )};
+
+function render(bookData) {
+    $('div').html(`
+    <div>
+        <h2>Title: ${bookData.title}
+    `)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+});
