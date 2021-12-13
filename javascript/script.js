979f06d01f4283b4d01ae55ba6d33520dc1e9dc0
ROBY MARTINEZ <=MARTINEZ.ROBYP@gmail.com>
2021-12-13 14:08:02 -0700

    gets input to show on DOM

--- javascript/script.js
+++ javascript/script.js
@@ -12,27 +12,28 @@ $button.on('submit', bookSearch)
 function bookSearch(evt) {
     evt.preventDefault();
     const userInput = $input.val();
+    $input.val('');
     $.ajax(URL + userInput).then(function (data){
-        render(data);
+        display(data) 
     }, function (error) {
         console.log(error);
+        console.log('oh no you have an error')
     }
     )};
 
-function render(bookData) {
-    $('div').html(`
-    <div>
-        <h2>Title: ${bookData.title}
-    `)
-}
+    function display(data){
+        // for (let i = 0; i < data.items.length; i++){
+            let $bookInfo = data.items[0];
+        $('div').html(`
+        <div>
+            <h2>Title: ${$bookInfo.volumeInfo.title}</h2>
+            <h2>Author: ${$bookInfo.volumeInfo.authors}</h2>
+            <h3>Published: ${$bookInfo.volumeInfo.publishedDate}</h3>
+        </div>`)
+        console.log($bookInfo.volumeInfo);
+    }
 
-
-
-
-
-
-
-
+// }
 
 
 
