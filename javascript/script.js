4584fa16a2baa9f3c2aee199994f2993641854fc
ROBY MARTINEZ <=MARTINEZ.ROBYP@gmail.com>
2021-12-14 12:59:28 -0700

    adds font styling and removes book cover img

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,40 @@
-$(function(){
+$(function () {
 
-const URL = "https://www.googleapis.com/books/v1/volumes?q="
+    const URL = "https://www.googleapis.com/books/v1/volumes?q="
 
 
-const $input = $('input[type="text"]');
-const $button = $('form');
+    const $input = $('input[type="text"]');
+    const $button = $('form');
 
-$button.on('submit', bookSearch)
+    $button.on('submit', bookSearch)
 
-// console.log($input)
-function bookSearch(evt) {
-    evt.preventDefault();
-    const userInput = $input.val();
-    $input.val('');
-    $.ajax(URL + userInput).then(function (data){
-        display(data) 
-    }, function (error) {
-        console.log(error);
-        console.log('oh no you have an error')
-    }
-    )};
+    // console.log($input)
+    function bookSearch(evt) {
+        evt.preventDefault();
+        const userInput = $input.val();
+        if (!userInput) return;
+        $input.val('');
+        $.ajax(URL + userInput).then(function (data) {
+            display(data)
+        }, function (error) {
+            console.log(error);
+            console.log('oh no you have an error')
+        }
+        )
+    };
 
-    function display(data){
-            let $bookInfo = data.items[0];
+    function display(data) {
+        let $bookInfo = data.items[0];
         $('div').html(`
-        <div class id="results">
+        <main class ="results">
+            
             <h2>${$bookInfo.volumeInfo.title}</h2>
             <h3>${$bookInfo.volumeInfo.authors}</h3>
             <p>Published: ${$bookInfo.volumeInfo.publishedDate}</p>
             <p id ="description">${$bookInfo.volumeInfo.description}</p>
-            <p id ="image"><img src="${$bookInfo.volumeInfo.imageLinks.thumbnail}"</p>
-        </div>`)
+            <p> <a href="${$bookInfo.volumeInfo.infoLink}" target ="#">More info</a></p>
+        </main>`)
         console.log($bookInfo.volumeInfo);
     }
-
-
-
-
+ 
 });
