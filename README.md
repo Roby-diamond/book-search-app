d089a6147ea9fdfaf4c6b774cba2d923dc5f41c4
ROBY MARTINEZ <=MARTINEZ.ROBYP@gmail.com>
2021-12-15 11:18:58 -0700

    adds README and cleans up code

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# book-search-app
+# Real Fake Books
+
+A book search app that uses both the Google Books and OpenLibrary APIs
+
+## Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+* jQuery
+* Google Fonts
+* Google Books API
+* OpebLibrary API
+
+## Screenshots
+
+
+
+## Getting Started
+
+
+## Future Enhancements
--- javascript/script.js
+++ javascript/script.js
@@ -29,13 +29,13 @@ $(function () {
         let $isbn = $bookInfo.volumeInfo.industryIdentifiers[1].identifier
         console.log($isbn);
         $('div').html(`
-        <main class ="results">
-        <p><img src="https://covers.openlibrary.org/b/isbn/${$isbn}-L.jpg" /></p>
+        <main>
+            <img src="https://covers.openlibrary.org/b/isbn/${$isbn}-L.jpg" />    
             <h2>${$bookInfo.volumeInfo.title}</h2>
             <h3>${$bookInfo.volumeInfo.authors}</h3>
             <p>Published: ${$bookInfo.volumeInfo.publishedDate}</p>
-            <p id ="description">${$bookInfo.volumeInfo.description}</p>
-            <p> <a href="${$bookInfo.volumeInfo.infoLink}" target ="#">More info</a></p>
+            <p>${$bookInfo.volumeInfo.description}</p>
+            <p><a href="${$bookInfo.volumeInfo.infoLink}" target ="#">More info</a></p>
         </main>`)
         
         
